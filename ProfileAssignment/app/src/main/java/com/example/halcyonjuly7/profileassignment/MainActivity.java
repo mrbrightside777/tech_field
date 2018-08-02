1e87efb78c71a98eb0240df63411f77ee88b4ca5
Halcyon Ramirez <halcyonjuly7@gmail.com>
2018-08-02 11:30:47 -0700

    profile project

---
+++ ProfileAssignment/app/src/main/java/com/example/halcyonjuly7/profileassignment/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.halcyonjuly7.profileassignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+//    private TextView name_val;
+//    private TextView dob_val;
+//    private TextView education_val;
+//    private TextView experience_val;
+//    private TextView hobbies_val;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
